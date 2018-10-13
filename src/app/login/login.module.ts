fea4c0ba2cf4883be393c926949e2513285c5908
Pere Zamora <pere.zamora@atmira.com>
2018-10-13 14:54:30 +0200

    Add Login Form

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule} from './login-routing.module';
 
@@ -8,7 +9,8 @@ import { LoginComponent } from './login.component';
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginComponent]
 })
